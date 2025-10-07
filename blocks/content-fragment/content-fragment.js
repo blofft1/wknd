abfbc13c7e99ac5a527ff808a5ba0de8ca6c598e
Prafful Singhal <praffuls@Praffuls-MacBook-Pro-2.local>
2025-10-07 16:00:22 +0530

    Update GraphQL endpoint references in blocks

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -12,7 +12,7 @@ export default async function decorate(block) {
   const CONFIG = {
     WRAPPER_SERVICE_URL: 'https://prod-60.eastus2.logic.azure.com:443/workflows/94ef4cd1fc1243e08aeab8ae74bc7980/triggers/manual/paths/invoke',
     WRAPPER_SERVICE_PARAMS: 'api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=e81iCCcESEf9NzzxLvbfMGPmredbADtTZSs8mspUTa4',
-    GRAPHQL_QUERY: '/graphql/execute.json/wknd-universal/CTAByPath',
+    GRAPHQL_QUERY: '/graphql/execute.json/ref-demo-eds/CTAByPath',
     EXCLUDED_THEME_KEYS: new Set(['brandSite', 'brandLogo'])
   };
 	
@@ -24,7 +24,7 @@ export default async function decorate(block) {
 	
 	//const aempublishurl = getMetadata('publishurl') || '';
 	
-  const persistedquery = '/graphql/execute.json/wknd-universal/CTAByPath';
+  const persistedquery = '/graphql/execute.json/ref-demo-eds/CTAByPath';
 
 	//const properties = readBlockConfig(block);
  
